ceee1e9a79d0cdbede7484dc3c5ccffcc7af469f
Mike Schutte <schutte@clinc.com>
2019-03-24 20:35:22 -0400

    Add roll 3 and play hand responder

--- lib/yahtzee/responder.rb
+++ lib/yahtzee/responder.rb
@@ -6,23 +6,48 @@ module Yahtzee
     STATUSES = {
       not_started: [
         [:say, ->(_) { "Welcome to Yahtzee!" }],
-        [:ask, ->(_) {{prompt: "Press enter to begin!", key: :enter_start}}],
+        [:ask, ->(_) {{prompt: "Press enter to begin!", key: :start_game}}],
       ],
       roll_1: [
         [:say, ->(state) { "Roll 1: #{state.fetch(:current_hand)}" }],
+        [:say, ->(state) { "Your hand: #{state.fetch(:held_dice)}" }],
         [:ask, ->(_) {{prompt: "Which ones do you want?", key: :roll_selection, parser: ResponseParser.digits}}],
       ],
       roll_2: [
         [:say, ->(state) { "Roll 2: #{state.fetch(:current_hand)}" }],
+        [:say, ->(state) { "Your hand: #{state.fetch(:held_dice)}" }],
         [:ask, ->(_) {{prompt: "Which ones do you want?", key: :roll_selection, parser: ResponseParser.digits}}],
+      ],
+      roll_3: [
+        [:say, ->(state) { "Roll 3: #{state.fetch(:current_hand)}" }],
+        [:say, ->(state) { "Your hand: #{state.fetch(:held_dice)}" }],
+        [:ask, ->(_) {{prompt: "Which ones do you want?", key: :roll_selection, parser: ResponseParser.digits}}],
+      ],
+      play_hand: [
+        [:say, ->(state) { "Play your hand: #{state.fetch(:held_dice)}" }],
+        [:say, ->(state) {
+          state.fetch(:hands)
+            .map { |(h, p)| "#{h}: #{p}" }.join("\n")
+        },],
+        [:ask, ->(_) {
+          {
+            prompt: "Which hand do you want to play",
+            key: :select_hand,
+          }
+        },],
       ],
     }
 
     def self.respond(state)
-      STATUSES.fetch(
-        state.fetch(:status),
-        [[:ask, ->(_) {{prompt: "Sorry, didn't catch that", key: :error_response}}]]
-      ).map { |(type, response)| [type, response.call(state)] }
+      [state.fetch(:error)]
+        .reject(&:nil?)
+        .map { |error| [:say, error] }
+        .concat(
+          STATUSES.fetch(
+            state.fetch(:status),
+            [[:ask, ->(_) {{prompt: "Sorry, didn't catch that", key: :error_response}}]]
+          ).map { |(type, response)| [type, response.call(state)] }
+        )
     end
   end
 end
